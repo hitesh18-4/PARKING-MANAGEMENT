537cafe9b1c2552d68a1ef649bac44a2b20406db
hitesh18-4 <hiteshbatra184@gmail.com>
2024-11-20 10:30:06 +0530

    Added

---
+++ client/src/App.js
@@ -0,0 +1,19 @@
+import logo from './logo.svg';
+import './App.css';
+import Navbar from './Components/Navbar';
+import SignUp from './Components/SignUp';
+import ParkingSpace from './Components/ParkingSpce';
+import Footer from './Components/Footer';
+function App() {
+  return (
+    <div>
+      
+      <Navbar />
+      <SignUp />
+      <ParkingSpace />
+      <Footer />
+    </div>
+  );
+}
+
+export default App;
